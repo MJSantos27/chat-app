6f46ae6ed6e88027fa6fffc54f0c88bdca0194c6
Miguel Santos <migueljsantos27@gmail.com>
2020-10-15 08:52:58 +0200

    add new name

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Contact.css';
-
 function Contact() {
+  const name = "Lori Pearson";
+  const avatar = "https://randomuser.me/api/portraits/women/66.jpg";
+  const online = true;
   return (
     <div className="Contact">
-     <img className="avatar" src = "https://randomuser.me/api/portraits/women/84.jpg" />
+     <img className="avatar" src = "https://randomuser.me/api/portraits/women/66.jpg" />
      <div>
-       <h4 className="name">Lori Pearson</h4>
-       <div className="status">
-       <span className="status-online">online</span>
+       <h4 className = "name">Edith Reid</h4>
+       <div className = "status">
+       <span className = "status-online"></span>
+       <span className = "status-text">online</span>
        </div>
       </div>
     </div>
