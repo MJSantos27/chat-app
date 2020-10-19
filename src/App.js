0192eababc0795408fcdd89269e728ae75ba2a7d
Miguel Santos <migueljsantos27@gmail.com>
2020-10-19 20:19:53 +0200

    changing fils

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from "react";
-import Contact from "./components/Contact.js";
+import ContactList from "./components/ContactList";
 
+const contacts = [
+  {
+    name: "Edith Reid",
+    avatar: "https://randomuser.me/api/portraits/women/66.jpg",
+    online: false,
+    Key: 1
+  },
+  {
+    name: "Nellie Caldwell",
+    avatar: "https://randomuser.me/api/portraits/women/17.jpg",
+    online: true,
+    Key: 2
+  },
+  {
+    name: "Gloria Allen",
+    avatar: "https://randomuser.me/api/portraits/women/88.jpg",
+    online: true,
+    Key: 3
+  },
+  {
+    name: "Erica Hunt",
+    avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+    online: false,
+    Key: 4
+  },
+  {
+    name: "Juanita Phillips",
+    avatar: "https://randomuser.me/api/portraits/women/55.jpg",
+    online: true,
+    Key: 5
+  }
+];
 function App() {
   return (
     <div className="App">
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/66.jpg"
-        name="Edith Reid"
-        online={true}
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/88.jpg"
-        name="Gloria Allen"
-        online={false}
-      />
-      <Contact
-        avatar="https://randomuser.me/api/portraits/women/74.jpg"
-        name="Sharlene Perry"
-        online={true}
-      />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
